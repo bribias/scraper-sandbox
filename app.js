5b5ae2994313f8a73046f9ffe50b787b9b354edc
Brianna Bias <brianna.bias@gmail.com>
2022-03-01 05:11:45 -0800

    send HTTP request

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const express = require('express');
+const axios = require('axios');
+
 const app = express();
 
 const PORT = process.env.port || 3000;
+
+const website = 'https://en.wikiquote.org/wiki/Buffy_the_Vampire_Slayer';
+
+try {
+    axios(website).then((response) => {
+        const html = response.data;
+        console.log(html);
+    });
+} catch (error) {
+    console.log(error, error.message);
+}
 
 app.listen(PORT, () => {
     console.log(`server is running on PORT:${PORT}`);

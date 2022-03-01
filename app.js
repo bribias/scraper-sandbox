ca8a745039e585b4ff82c6b8234335a49eedc246
Brianna Bias <brianna.bias@gmail.com>
2022-03-01 05:53:13 -0800

    grabs buffy episode list

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const axios = require('axios');
+const cheerio = require('cheerio');
 
 const app = express();
 
@@ -8,9 +9,24 @@ const PORT = process.env.port || 3000;
 const website = 'https://en.wikiquote.org/wiki/Buffy_the_Vampire_Slayer';
 
 try {
-    axios(website).then((response) => {
-        const html = response.data;
-        console.log(html);
+    axios(website).then((res) => {
+        const data = res.data;
+        const $ = cheerio.load(data);
+
+        let content = [];
+
+        $('.mw-headline', data).each(function () {
+            const episode = $(this).text();
+            const url = $(this).find('a').attr('href');
+
+            content.push({
+                episode,
+                url,
+            });
+            app.get('/', (req, res) => {
+                res.json(content);
+            });
+        });
     });
 } catch (error) {
     console.log(error, error.message);
